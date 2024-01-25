11d827fcb9a1a83920a9371038521fed8120f98b
Fernando Diaz <fediaz.dev@gmail.com>
2024-01-25 17:30:31 -0500

    added camera, viewport, and ray struct

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,73 @@
 mod color;
+mod ray;
 mod vec3;
 
 use color::Color;
+use ray::Ray;
+use vec3::{dot, unit_vector, Point3, Vec3};
+
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - *center;
+    let a = r.direction().length_squared();
+    let half_b = dot(&oc, &r.direction());
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discriminant.sqrt()) / a
+    }
+}
+
+fn ray_color(r: &Ray) -> Color {
+    let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
+    }
+
+    let unit_direction = unit_vector(r.direction());
+    let a = 0.5 * (unit_direction.y + 1.0);
+
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let image_height = if image_height < 1 { 1 } else { image_height };
+
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let true_aspect_ratio = image_width as f64 / image_height as f64;
+    let viewport_width = viewport_height * true_aspect_ratio;
+    let camera_center = Vec3::new(0.0, 0.0, 0.0);
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     println!("P3\n{image_width} {image_height}\n255");
 
     for j in 0..image_height {
         eprint!("\rScanlines remaining: {}", image_height - j);
         for i in 0..image_width {
-            let pixel_color = Color {
-                x: i as f64 / (image_width - 1) as f64,
-                y: j as f64 / (image_height - 1) as f64,
-                z: 0.0,
-            };
+            let pixel_center =
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+
+            let pixel_color = ray_color(&r);
             color::write_color(&pixel_color);
         }
     }
